585affc2e67b3b3970ea03354488c9f37aec101f
ABHIDAS-S <abhidasabhi1234@gmail.com>
2024-10-30 09:18:59 +0530

    api chanaged

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "http://localhost:9000/api",
-  withCredentials: true
-  // baseURL:"https://www.erp.camet.in/"
+  // baseURL: "http://localhost:9000/api",
+  withCredentials: true,
+  baseURL:"https://www.erp.camet.in/"
 })
 export default api
