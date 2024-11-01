e33ed09d404995d4c28ef8eeb4b6b384929fc44f
ABHIDAS-S <abhidasabhi1234@gmail.com>
2024-11-01 13:33:31 +0530

    leaveappiction removed from headers not completed

--- frontend/src/router/CrmRoutes/childRoutes/transactionsRoutes.js
+++ frontend/src/router/CrmRoutes/childRoutes/transactionsRoutes.js
@@ -1,11 +1,11 @@
-import LeaveApplication from "../../../components/primaryUser/LeaveApplication"
+// import LeaveApplication from "../../../components/primaryUser/LeaveApplication"
 import ComingSoon from "../../../pages/common/ComingSoon"
 import CallRegistration from "../../../pages/primaryUser/register/CallRegistration"
 const transactionsRoutes = [
   { path: "/admin/transaction/lead", component: ComingSoon },
   { path: "/admin/transaction/call-registration", component: CallRegistration },
 
-  { path: "/admin/transaction/leave-application", component: LeaveApplication }
+  { path: "/admin/transaction/leave-application", component: ComingSoon }
 ]
 
 export default transactionsRoutes
--- frontend/src/router/staffRoutes/stafftransactionRoutes.js
+++ frontend/src/router/staffRoutes/stafftransactionRoutes.js
@@ -1,10 +1,10 @@
-import LeaveApplication from "../../components/primaryUser/LeaveApplication"
+// import LeaveApplication from "../../components/primaryUser/LeaveApplication"
 import CallRegistration from "../../pages/primaryUser/register/CallRegistration"
 import ComingSoon from "../../pages/common/ComingSoon"
 const stafftransactionsRoutes = [
   { path: "/staff/transaction/lead", component: ComingSoon },
   { path: "/staff/transaction/call-registration", component: CallRegistration },
-  { path: "/staff/transaction/leave-application", component: LeaveApplication }
+  { path: "/staff/transaction/leave-application", component: ComingSoon }
 ]
 
 export default stafftransactionsRoutes
