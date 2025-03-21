b600873fa6d79d9e1bd34f61580d44bf4d4c99b0
ABHIDAS-S <abhidasabhi1234@gmail.com>
2025-03-21 12:17:28 +0530

    leavesummary search backspace issue solved

--- frontend/src/components/primaryUser/LeaveSummary.jsx
+++ frontend/src/components/primaryUser/LeaveSummary.jsx
@@ -465,7 +465,13 @@ const leaveSummary = () => {
               value={
                 selectedStaff !== null ? selectedStaff[0].name : searchTerm
               }
-              onChange={(e) => setSearchTerm(e.target.value)}
+              onChange={(e) => {
+                if (selectedStaff !== null) {
+                  setSelectedIndex(null)
+                  setselectedStaff(null) // Clear selected staff
+                }
+                setSearchTerm(e.target.value)
+              }}
               placeholder="Search..."
               className="appearance-none rounded-r rounded-l sm:rounded-lg-none border border-gray-400 border-b block pl-8 pr-6 py-2 w-auto bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none"
             />
