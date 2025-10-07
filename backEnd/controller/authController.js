c052c0a850c4be8af8eac869301d9b18f0dfb0e2
ABHIDAS-S <abhidasabhi1234@gmail.com>
2025-10-07 11:08:51 +0530

    branch selected add in getsomeall

--- backEnd/controller/authController.js
+++ backEnd/controller/authController.js
@@ -1149,7 +1149,7 @@ export const OnsiteApply = async (req, res) => {
 
 export const GetsomeAll = async (req, res, yearParam = {}, monthParam = {}) => {
   try {
-    const { year, month } = req.query || { year: yearParam, month: monthParam }
+    const { year, month} = req.query || { year: yearParam, month: monthParam }
     // console.log("selectedBranch", selectedBranch)
 
     function getSundays(year, month) {
@@ -1237,7 +1237,7 @@ export const GetsomeAll = async (req, res, yearParam = {}, monthParam = {}) => {
     const sundayFulldate = createDates(sundays, month, year)
     const startDate = new Date(Date.UTC(year, month - 1, 1))
     const endDate = new Date(Date.UTC(year, month, 0))
-   
+    
     const users = await Staff.aggregate([
       {
         $match: { isVerified: true }
