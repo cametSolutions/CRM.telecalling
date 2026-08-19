d1bcf55438e7aaba55ca963c9828e2975c3b6f69
ABHIDAS-S <abhidasabhi1234@gmail.com>
2026-08-19 10:31:31 +0530

    added monthly holidays

--- frontend/src/pages/primaryUser/dashboard/PrimaryUserDashBoard.jsx
+++ frontend/src/pages/primaryUser/dashboard/PrimaryUserDashBoard.jsx
@@ -75,7 +75,7 @@ console.log(announcementlist)
           date.getFullYear() === currentYear && date.getMonth() === currentMonth
         )
       })
-
+console.log(currentMonthHolidays)
       setcurrentyearHoliday(currentMonthHolidays)
     }
   }, [holydata])
