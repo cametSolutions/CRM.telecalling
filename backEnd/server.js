470b73119f85704c908b59888a9cba3ea9dade72
ABHIDAS-S <abhidasabhi1234@gmail.com>
2026-09-14 14:58:52 +0530

    .env change to production in frontend

--- backEnd/server.js
+++ backEnd/server.js
@@ -656,6 +656,7 @@ app.use((err, req, res, next) => {
 /* ===============================
    PROD / DEV
 ================================ */
+console.log("proces.env",process.env.NODE_ENV)
 if (process.env.NODE_ENV === "production") {
   const parentDir = path.join(__dirname, "..")
   app.use(express.static(path.join(parentDir, "frontend/dist")))
