93d666ba4275a24c535a253a66c631b8395b85b0
ABHIDAS-S <abhidasabhi1234@gmail.com>
2026-09-14 11:36:38 +0530

    collection update bugs cleared

--- frontend/src/router/CrmRoutes/childRoutes/homeRoutes.js
+++
@@ -1,9 +0,0 @@
-// import HomePage from '../pages/HomePage';
-// import AboutPage from '../pages/AboutPage';
-
-// const homeRoutes = [
-//   { path: '/', component: HomePage },
-//   { path: '/about', component: AboutPage }
-// ];
-
-// export default homeRoutes;
