85f9722895a339afbcb5d0e471ed65d8cc08ef0e
ABHIDAS-S <abhidasabhi1234@gmail.com>
2026-09-16 12:56:23 +0530

    expired customer

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 let baseURL
 const ENV = import.meta.env.VITE_NODE_ENV
+console.log("env",ENV)
 if (ENV === "development") {
   baseURL = "http://localhost:9000/api"
 } else if (ENV === "production") {
